05a788b1249b9a2a23a25a7c79fcfeea43c48119
miki kinjyo <kinjyo@razona.jp>
2021-03-09 20:02:01 +0900

    [add] gatsby 初期ファイル

---
+++ photoshop-tutorial/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function TOP() {
+  return (
+    <Layout>
+      <div>
+        <p>トップページのメインコンテンツ！</p>
+      </div>
+    </Layout>
+  )
+}
