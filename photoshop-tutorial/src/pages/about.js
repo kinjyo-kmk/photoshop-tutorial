200166e3861ef45cc1ab818a1e7cb3f293d81d3b
miki kinjyo <kinjyo@razona.jp>
2021-03-09 22:20:39 +0900

    [add] meta設定

--- photoshop-tutorial/src/pages/about.js
+++ photoshop-tutorial/src/pages/about.js
@@ -1,12 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import Meta from "../components/meta"
 
 export default function TOP() {
   return (
     <Layout>
+			<Meta
+				title="サブ"
+				desc="Gatsby.jsの使い方を説明するためのサンプルページです。"
+			/>
       <div>
         <p>概要ページです。</p>
+        <Link to={"/"}>TOP</Link>
       </div>
     </Layout>
   )
